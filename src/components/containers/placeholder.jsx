149a4a3e3be9238c0b77e9b974efed754a222161
Stephen Fairris <stephen@stelsey.com>
2019-02-18 14:53:49 -0800

    completed placeholder component

--- src/components/containers/placeholder.jsx
+++ src/components/containers/placeholder.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { secondaryColor, logoFont } from '../../util/theme'
 
 class Placeholder extends Component {
   constructor(props) {
@@ -25,7 +26,7 @@ class Placeholder extends Component {
   componentDidMount() {
     const { _handleImageChange, _handleImageFade } = this
 
-    setInterval(_handleImageChange, 7100)
+    setInterval(_handleImageChange, 7050)
 
     setInterval(_handleImageFade, 50)
   }
@@ -55,23 +56,71 @@ class Placeholder extends Component {
   render() {
     const { images, imageCounter, opacity } = this.state
 
-  const styles = {
-    image: {
-      padding: '0px',
-      opacity: opacity
+    const styles = {
+      container: {
+        position: 'relative'
+      },
+      image: {
+        opacity: opacity,
+        position: 'absolute',
+        padding: 'none',
+        margin: 'none',
+        pointerEvents: 'none'
+      },
+      centered: {
+        textAlign: 'center',
+        paddingTop: '400px'
+      },
+      logo: {
+        fontFamily: logoFont,
+        fontSize: '100px',
+        color: secondaryColor,
+        fontWeight: '300',
+        marginBottom: '0px'
+      },
+      subHeader: {
+        fontFamily: logoFont,
+        color: 'lightGrey',
+        fontSize: '40px',
+        marginTop: '0px'
+      },
+      email: {
+        color: secondaryColor,
+        fontFamily: logoFont,
+        fontSize: '25px',
+        textDecoration: 'none'
+      },
+      icon: {
+        height: '50px',
+        marginTop: '20px'
+      }
     }
-  }
 
     return(
-      <div>
+      <div style={styles.container}>
         <img
           src={images[imageCounter]}
           style={styles.image}
+          alt='background'
         />
+        <div style={styles.centered}>
+          <h1 style={styles.logo}> 22 Studios</h1>
+          <h2 style={styles.subHeader}> Coming soon</h2>
+          <a style={styles.email} href='mailto:photography@22-studios.com'> photography@22-studios.com</a>
+          <br />
+          <a href='https://www.instagram.com/22_._studios/' target='_blank' rel='noopener noreferrer'>
+          <img
+            src='https://res.cloudinary.com/df0ll615k/image/upload/v1550529258/insta-icon-green.png'
+            style={styles.icon}
+            alt='instagram'
+          />
+          </a>
+        </div>
       </div>
     )
 
   }
 }
+
 
 export default Placeholder
--- src/util/theme.js
+++ src/util/theme.js
@@ -1,3 +1,4 @@
-export const primary = '#6b5b95'
-export const secondary = '#a2b9bc'
-export const nuetral = '#878f99'
+export const primaryColor = '#6b5b95'
+export const secondaryColor = '#a2b9bc'
+export const nuetralColor = '#878f99'
+export const logoFont = 'Dancing Script'
